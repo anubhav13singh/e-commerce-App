26351e10f37e61e01af38686bd3f5092eb1705e4
Anubhav Singh <singhanubhav81717@gmail.com>
2023-10-04 09:53:57 +0530

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,64 @@
-import React, { useState } from 'react'
-import { CgMenu, CgShoppingCart} from 'react-icons/cg'
-import { NavLink } from 'react-router-dom'
-import './header.css'
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { Box, useMediaQuery } from "@mui/material";
 
-function Header() {
+const ResponsiveNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMobile = useMediaQuery("(max-width:700px)");
 
-  const [toggle, setToggle] = useState(false);  
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
-    <div className='header'>
-      <h2>Royal <span>Bazaar</span></h2>
-     
-     <div className={toggle? 'mobile-nav': 'nav'}>
-      
-      <NavLink to={'/'}><li>Home</li></NavLink> 
-      <NavLink to={'/About'}><li>About</li></NavLink> 
-      <NavLink to={'/Product'}><li>Products</li></NavLink> 
-      <NavLink to={'/Contact'}><li>Contact</li></NavLink> 
-      <NavLink style={{position:'relative'}} to={'/Cart'}><li><CgShoppingCart/> <span  style={{
-      fontSize:'18px', position:'absolute',right:'-1px', bottom:'14px', color:'red'}}>10</span>
-      </li></NavLink> 
-            
-      </div> 
+    <AppBar sx={{ position: "sticky", top: "0px", left: "0px" }}>
+      {/* Toolbar component is a layout component that is typically used as a container for other UI components within an app's top app bar or toolbar. It's often used in conjunction with the AppBar component to create the top navigation bar of an application. */}
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Navbar
+        </Typography>
+        {isMobile ? (
+          <>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenuClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+              <MenuItem onClick={handleMenuClose}>About</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Services</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <>
+            <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+            <MenuItem onClick={handleMenuClose}>About</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Services</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-      <i className='menu' onClick={() => setToggle(!toggle)}><CgMenu/></i>     
-               
-    </div>
-    
-     
-    </>
-                
-                
-  )
-}
-
-export default Header;
+export default ResponsiveNavbar;
